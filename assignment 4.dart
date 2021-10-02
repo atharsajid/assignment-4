3141689a5987fa8f6a9b34b99304b71568d0f339
atharsajid <athersajid620@gmail.com>
2021-10-02 01:11:37 -0700

    update

--- assignment 4.dart
+++ assignment 4.dart
@@ -1,67 +1,84 @@
-void main(List<String> args) {
+//Question NO 2
+void main() {
+  table(5);
+}
 
-  //Question NO 2
-table(5);
+void table(int num) {
+  for (var i = 0; i <= 10; i++) {
+    print("$num * $i = ${num * i}");
+  }
+}
 
 // question no 3
-divide(10);
-
-// Question NO 2
-var name =["Ali","Ahmed","Bilal","Faraz","Fahad"];
-var marks=[255,425,411,345,145];
-var percen = [];
-for (var x in marks) {
-  var a = (x*100) ~/ 500;
-  percen.add(a);
-}
-for (var i = 0; i < name.length; i++) {
-  print("${name[i]} obtain marks is ${marks[i]} out of 500 and Got ${percen[i]}% " );
-  marksheet(percen[i]);
-  print("");
-
-
+void main() {
+  divide(10);
 }
 
-
-
-
-}
-  //Question NO 2
-
-void table(int num){
-  for (var i = 0; i <=10; i++) {
-    print("$num * $i = ${num*i}");
-  }
-
-}
-  //Question NO 3
-
-void divide(num d){
-  var list =[2,4,6,8,9];
+void divide(num d) {
+  var list = [2, 4, 6, 8, 9];
   for (var x in list) {
-    var a = d/x;
+    var a = d / x;
     print(a);
   }
 }
 
-  //Question NO 4
-void marksheet(var per){
-  if (per >=80) {
+// Question NO 4
+void main() {
+  var name = ["Ali", "Ahmed", "Bilal", "Faraz", "Fahad"];
+  var marks = [255, 425, 411, 345, 145];
+  var percentage = [];
+  for (var x in marks) {
+    var a = (x * 100) ~/ 500;
+    percentage.add(a);
+  }
+  for (var i = 0; i < name.length; i++) {
+    print(
+        "${name[i]} obtain marks is ${marks[i]} out of 500 and Got ${percentage[i]}% ");
+    marksheet(percentage[i]);
+    print("");
+  }
+}
+
+void marksheet(var per) {
+  if (per >= 80) {
     print("A+ grade");
-  }
-  else if (per >=70) {
+  } else if (per >= 70) {
     print("A grade");
-  }
-  else if (per >=60) {
+  } else if (per >= 60) {
     print("B grade");
-  }
-  else if (per >=50) {
+  } else if (per >= 50) {
     print("C grade");
-  }
-  else if (per >=40) {
+  } else if (per >= 40) {
     print("Pass");
-  }else{
+  } else {
     print("Fail");
   }
+}
 
+// Question NO 5
+void main() {
+  Abc object = Abc();
+  object.fun1();
+}
+
+class Abc {
+  void fun1() {
+    print("hello world");
+  }
+}
+
+// Question NO 6
+void main() {
+  Name obje = Name();
+  //write your name here
+  obje.name = "Athar";
+
+  obje.fun();
+}
+
+class Name {
+  late String name;
+  void fun() {
+    print("My name is $name");
+  }
 }
